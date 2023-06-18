0f2570e5bf0eb27a6bc3a048246dc49bc65dc2c3
preet <PatelPreet0812@gmail.com>
2023-06-18 22:33:29 +0530

    check commit

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,14 +1,8 @@
-const User = require('./../models/User')
-const { authenticator } = require('otplib')
-const { validationResult}=require('express-validator')
 const { createUser } = require('../services/user.services')
 
 exports.register = async (req, res, next) => {
-    
     const {message,statusCode,Type,data}=await createUser(req)
-    console.log("message,statusCode,Type,data",message,statusCode,Type,data);
-    res.status(statusCode).json({"data":data})
-   
+    res.status(statusCode).json({"message":message,"data":data}) 
 }
 exports.update=(req,res,next)=>{
 
--- helpers/helper.js
+++ helpers/helper.js
@@ -1,10 +1,28 @@
 const User = require("../models/User")
 
+const jwt=require('jsonwebtoken')
+
 exports.checkUser = async (email) => {
     const user = await User.find({ email })
+    console.log("user",user);
+    const secret_key = process.env.JWT_SECRET
+    const token = jwt.sign({ user: user }, secret_key);
     if (user.length) {
-        return true
+        return {
+            statusCode: 200,
+            type: "Success",
+            data: {
+                user: user,
+                token: token
+            },
+            messsage:"Login successfully !!!"
+        }
     } else {
-        return false
+        return {
+            statusCode: 400,
+            type: "Error",
+            data: [],
+            messsage:"User not found"
+        }
     }
 }
